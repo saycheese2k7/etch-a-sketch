5802f75b9c21c7824cb0acc91d67e367c379230d
saycheese2k7 <241291587+saycheese2k7@users.noreply.github.com>
2025-11-15 17:03:13 +0600

    barebones halfway done

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ let takenGrid
 
 preferredGrid = prompt("Enter a number between 1 and 64");
 
-if(typeof preferredGrid !== 'number' || preferredGrid <= 1 || preferredGrid >= 64){
+/*if(typeof preferredGrid !== 'number' || preferredGrid <= 1 || preferredGrid >= 64){
     throw new Error('Input must be a NUMBER between 1 and 64');
-}
+}*/
 
 takenGrid = parseInt(preferredGrid);
 
-const verticalCont = document.getElementById('vertcontainer')
+const verticalCont = document.getElementById('mainbox')
 
-for(let i = 0; i <= takenGrid; i++){
-    const horizontalDiv = document.createElement('div')
+for(let i = 0; i < takenGrid; i++){
+    let horizontalDiv = document.createElement('div')
     horizontalDiv.classList.add('hrzdiv')
-    horizontalDiv.style.display('inline-block')
-    horizontalDiv.style.padding('5px')
-    horizontalDiv.style.
+    horizontalDiv.style.display = 'flex'
+    horizontalDiv.style.flexDirection = 'row'
+    horizontalDiv.style.flex = '1'
+    horizontalDiv.style.gap = '0'
+    horizontalDiv.style.justifyContent = 'space-between'
+    horizontalDiv.style.height = '20px'
+
 
     verticalCont.appendChild(horizontalDiv)
 }
